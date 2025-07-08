e6c57f79bc4e97a6c986ba3a8da8679f76eb2198
Lunado <liendhhha140217@gmail.com>
2025-07-08 22:23:34 +0900

    Setup project

---
+++ src/main/java/com/example/ManageLocation/ManageLocationApplication.java
@@ -0,0 +1,13 @@
+package com.example.ManageLocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ManageLocation/ManageLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ManageLocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
